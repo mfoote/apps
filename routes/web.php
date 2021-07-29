648ebd2c0ab374e40f83cf389456b8a1d11714ff
Mike Foote <mike.foote67@gmail.com>
2021-07-29 09:15:00 -0400

    testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/landing/fbss', function () {
+    return view('landing_pages.fbss');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
